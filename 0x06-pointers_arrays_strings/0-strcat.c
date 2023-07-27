c39dee740dcb4de2c76977702d80d1a57d9af4d2
SE-forte <babegifty@icloud.com>
2023-07-27 06:15:48 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,21 +6,23 @@
  * Return: pointer to destination string.
  */
 
-void _strcat(char *dest, const char *src)
+char *_strcat(char *dest, char *src)
 {
 	int length_of_string, z;
 
-	int j = 0;
-
 	length_of_string = 0;
+
+	int j, j = 0;
+
 	while (dest[length_of_string] != '\0')
 	{
 		length_of_string++;
 	}
-	for (z = 0; src[z] != '\0'; z++, length_of_string++)
+	for (z = 0; src[z] != '\0'; length_of_string++)
+
+		j = 0;
 	{
 		dest[length_of_string] = src[j];
-		_strcat(dest, src);
 	}
 	dest[length_of_string] = '\0';
 	return (dest);
